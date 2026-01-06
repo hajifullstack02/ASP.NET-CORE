e55faae12508b5560e13f984b6e29037e3a91366
hajifullstack@02 <haji.fullstack@gmail.com>
2026-01-06 22:19:10 +0530

    httpcontext

---
+++ WebApplication5/WebApplication5/Program.cs
@@ -0,0 +1,43 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors((cors) =>
+{
+    cors.AddPolicy("AllowLocalhost4200", policy =>
+    {
+        policy.
+        WithOrigins("http://localhost:4200").
+        AllowAnyHeader().
+        AllowAnyMethod();
+    });
+});
+
+//http://localhost:4200
+
+var app = builder.Build();
+//https://localhost:7115/api/Employees/GetEmpName
+
+
+.
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
